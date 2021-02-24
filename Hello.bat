b8af4098868d705c1f8833439ab7eec755b285d8
ShivamIND <79452700+ShivamIND@users.noreply.github.com>
2021-02-24 10:58:38 +0530

    Update Hello.bat

--- Hello.bat
+++ Hello.bat
@@ -1,3 +1,2 @@
-echo "This is a Shell Script and needs to be run on a Linux Agent!!"
-echo "Hello, today is $(date)"
-echo "In Master Branch"
+echo "Hello, today is 24th Feb"
+echo "In Feature Branch"
